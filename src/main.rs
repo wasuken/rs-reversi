9e53325a8283c811e74c1b9d4bee64fdacbb4f7e
wasuken <wevorence@gmail.com>
2020-04-19 13:53:15 +0900

    success unit tests.

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,92 @@ const SECOND_STR: &str = "*";
 // サイズ
 const SIZE: i32 = 8;
 // ターン
+#[derive(Debug)]
 #[derive(PartialEq)]
 enum Player {
 	FIRST,
 	SECOND
 }
-fn pickup_points(map: &Vec<Vec<&str>>, player: &Player) -> Vec<(i32, i32)> {
+// orientは向きを示す。具体的には以下の通り。
+// 1 2 3
+// 4 * 6
+// 7 8 9
+fn check_straight_line_same_piece_exist(map: &Vec<Vec<&str>>,
+										player: &Player,
+										x: usize,
+										y: usize,
+										ptr: (i32, i32)) -> bool {
+	let (x_, y_) = ptr;
+	let player_str = match player {
+		Player::FIRST => FIRST_STR,
+		Player::SECOND => SECOND_STR,
+	};
+	if (x as i32+x_) as usize > (SIZE as usize) ||
+		(y as i32+y_) as usize > (SIZE as usize) {
+			return false
+	}
 
-	return [].to_vec()
+	if map[(x as i32+x_) as usize][(y as i32+y_) as usize] == player_str {
+		println!("success");
+		return true
+	}else if map[(x as i32+x_) as usize][(y as i32+y_) as usize] == EMPTY_STR {
+		println!("empty");
+		return false
+	}else {
+		println!("next");
+		return check_straight_line_same_piece_exist(map,
+													player,
+													(x as i32+x_) as usize,
+													(y as i32+y_) as usize,
+													ptr)
+	}
+}
+fn pickup_points(map: &Vec<Vec<&str>>, player: &Player) -> Vec<(usize, usize)> {
+	let another_player_str = match player {
+		Player::FIRST => SECOND_STR,
+		Player::SECOND => FIRST_STR,
+	};
+	let mut result = Vec::new();
+	for x in 0..map.len() {
+		for y in 0..map[0].len() {
+			if map[x][y] == EMPTY_STR {
+				for i in -1i32..2 {
+					for j in -1i32..2 {
+						if (x as i32 + i) >= 0 && (x as i32 + i) < SIZE &&
+							(y as i32 + j) >= 0 &&
+							(y as i32 + j) < SIZE &&
+							map[(x as i32+i) as usize][(y as i32+j) as usize] == another_player_str {
+								// 該当方向の先にplayerと同じピースが存在するか。
+								let i_ = ((x as i32)+i) as usize;
+								let j_ = ((y as i32)+j) as usize;
+								if i_ < (SIZE as usize) && j_ < (SIZE as usize) &&
+									check_straight_line_same_piece_exist(&map, player, i_, j_, (i, j)) {
+										result.push((x, y));
+									}
+							}
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+fn is_skip(map: &Vec<Vec<&str>>, player: &Player) -> bool {
+	return pickup_points(map, player).len() <= 0
 }
 fn check_put_piece(map: &Vec<Vec<&str>>, x: usize, y: usize, player: &Player) -> bool {
 	// 打てる手を全て算出。
 	let ptrs = pickup_points(&map, player);
 	// 算出した手以外にピースを置いた場合、false。
+	for ptr in ptrs {
+		if ptr.0 == x && ptr.1 == y {
+			return true
+		}
+	}
 	return false
 }
 // ピースの配置
 fn put_piece(map: &mut Vec<Vec<&str>>, x: usize, y: usize, player: &Player) {
-	// check
-	if !check_put_piece(&map.to_vec(), x, y, player) {
-		println!("Foul move.");
-		return
-	}
 	let piece = match player {
 		Player::FIRST => FIRST_STR,
 		Player::SECOND => SECOND_STR
@@ -56,12 +120,9 @@ fn create_default_map() -> Vec<Vec<&'static str>> {
 	}
 	return map
 }
-fn is_skip(map: &Vec<Vec<&str>>, player: &Player) -> bool {
-	return false
-}
 fn is_finish(map: &Vec<Vec<&str>>) -> bool {
 	let e_cnt: usize = map.clone().into_iter()
-				.map(|x: Vec<&str>| x.iter().filter(|y| **y == EMPTY_STR).count())
+		.map(|x: Vec<&str>| x.iter().filter(|y| **y == EMPTY_STR).count())
 		.collect::<Vec<usize>>().into_iter().sum::<usize>();
 	if e_cnt == 0 {
 		return true
@@ -70,6 +131,7 @@ fn is_finish(map: &Vec<Vec<&str>>) -> bool {
 	}
 }
 fn main() {
+	env_logger::init();
 	// マップ
 	let mut map: Vec<Vec<&str>> = create_default_map();
 	// ターン変数
@@ -85,10 +147,13 @@ fn main() {
 				.collect::<Vec<usize>>().into_iter().sum::<usize>();
 			if f_cnt > s_cnt {
 				println!("winner is first.");
+				break
 			}else if f_cnt == s_cnt {
 				println!("draw.");
+				break
 			}else{
 				println!("winner is second.");
+				break
 			}
 		}
 		// Skip判定
@@ -112,6 +177,12 @@ fn main() {
 			println!("Failed input.");
 			break;
 		}
+		// check
+		if !check_put_piece(&map.to_vec(), ptrs[0].parse::<usize>().unwrap(),
+				  ptrs[1].parse::<usize>().unwrap(), &player) {
+			println!("Foul move.");
+			return
+		}
 		// マップ反映
 		put_piece(&mut map, ptrs[0].parse::<usize>().unwrap(),
 				  ptrs[1].parse::<usize>().unwrap(), &player);
@@ -132,12 +203,13 @@ mod tests {
 		// ピースが裏返らない手は打てない。
 		assert_eq!(false, check_put_piece(&map, 0, 0, &Player::FIRST));
 		// すでに置いてある場所には置けない。
-		assert_eq!(true, check_put_piece(&map, 3, 3, &Player::FIRST));
+		assert_eq!(false, check_put_piece(&map, 3, 3, &Player::FIRST));
     }
 	#[test]
 	fn test_is_skip(){
 		let mut map = create_default_map();
 		// 初期パターン。当然false。
+		assert_eq!(false, is_skip(&map, &Player::SECOND));
 		assert_eq!(false, is_skip(&map, &Player::FIRST));
 		// 全てのパターンが黒になった時。
 		put_piece(&mut map, 3, 4, &Player::FIRST);
@@ -162,7 +234,24 @@ mod tests {
 	#[test]
 	fn test_pickup_points(){
 		let map = create_default_map();
-		let expect = [(3, 2), (2, 3), (4, 5), (5, 4)].to_vec();
-		assert_eq!(expect, pickup_points(&map, &Player::FIRST));
+		let expect = [(2, 3), (3, 2), (4, 5), (5, 4)].to_vec();
+		assert_eq!(expect, pickup_points(&map, &Player::SECOND));
+		let expect2 = [(2, 4), (3, 5), (4, 2), (5, 3)].to_vec();
+		assert_eq!(expect2, pickup_points(&map, &Player::FIRST));
+	}
+	#[test]
+	fn test_check_straight_line_same_piece_exist(){
+		let map = create_default_map();
+		let mut map2 = map.clone();
+		put_piece(&mut map2, 3, 2, &Player::SECOND);
+		put_piece(&mut map2, 4, 5, &Player::SECOND);
+
+		let result1 = check_straight_line_same_piece_exist(&map, &Player::SECOND, 3, 2, (0, 1));
+		let result2 = check_straight_line_same_piece_exist(&map, &Player::SECOND, 4, 5, (0, -1));
+		let result3 = check_straight_line_same_piece_exist(&map, &Player::SECOND, 0, 0, (0, -1));
+
+		assert_eq!(true, result1);
+		assert_eq!(true, result2);
+		assert_eq!(false, result3);
 	}
 }
